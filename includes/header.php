138da9c2b994805339a2658ef734c9dae6117deb
Nisaa Dawi <154902841+nisaadawi@users.noreply.github.com>
2025-03-24 13:00:23 +0800

    code updates

---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<!--header-->
+    <div class="header" id="home" style="background-color: #c99a5b;">
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"> </span>
+            <span class="icon-bar"> </span>
+            <span class="icon-bar"> </span>
+          </button>
+          <style>
+            .logo {
+              margin-top: 20px; /* Adjust this value as needed */
+            }
+            .logo img {
+              background-color: #c99a5b;
+              border: 2px solid #c99a5b;
+            }
+          </style>
+          <div class="logo"><a href="index.php"><img src="images/icon_RSB.png"></a></div>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right margin-top cl-effect-2">
+                <li><a href="index.php"><span data-hover="Home">Home</span></a></li>
+                <li><a href="about.php"><span data-hover="Contact">About Us</span></a></li>
+                <li><a href="contact.php"><span data-hover="Contact">Contact</span></a></li>
+                <li><a href="login.php"><span data-hover="Contact">Login</span></a></li>
+                
+              </ul>
+              <div class="clearfix"> </div>
+            </div><!-- /.navbar-collapse -->
+        <!-- /.container-fluid -->
+
+      </nav>
+<!--/script-->
+       <div class="clearfix"> </div>
+</div>
+<!-- Top Navigation -->
+
+<!--header-->
